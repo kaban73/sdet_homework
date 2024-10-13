51e836cc6b2c29f5c68582cb0114670c78b0e344
Ivan Kabanov <i.borkoff@yandex.ru>
2024-10-13 14:05:54 +0300

    homework is completed

--- src/lection2/Authorization.java
+++ src/lection2/Authorization.java
@@ -1,11 +1,40 @@
 package lection2;
 
 public class Authorization {
+    private static void isConfirmPasswordEqualPassword(String password, String confirmPassword) throws WrongPasswordException {
+        if (!password.equals(confirmPassword))
+            throw new WrongPasswordException("Confirm password is not equal to the password");
+    }
+    private static void isLengthPasswordCorrect(String password) throws WrongPasswordException {
+        if (password.length() > 20)
+            throw new WrongPasswordException("Password is too long");
+        else if (password.isEmpty())
+            throw new WrongPasswordException("Password can not be empty");
+    }
+    private static void arePasswordCharactersCorrect(String password) throws WrongPasswordException {
+        String regex = "^[a-zA-Z0-9_]+$";
+        if (!password.matches(regex))
+            throw new WrongPasswordException("Password contains invalid characters");
+    }
+    private static void isLengthLoginCorrect(String login) throws WrongLoginException{
+        if (login.length() > 20)
+            throw new WrongLoginException("Login is too long");
+        else if (login.isEmpty())
+            throw new WrongLoginException("Login can not be empty");
+    }
+    private static void areLoginCharactersCorrect(String login) throws WrongLoginException {
+        String regex = "^[a-zA-Z0-9_]+$";
+        if (!login.matches(regex))
+            throw new WrongLoginException("Login contains invalid characters");
+    }
     private static void checkLogin(String login) throws WrongLoginException {
-
+        isLengthLoginCorrect(login);
+        areLoginCharactersCorrect(login);
     }
     private static void checkPasswords(String password, String confirmPasswords) throws WrongPasswordException {
-
+        isLengthPasswordCorrect(password);
+        arePasswordCharactersCorrect(password);
+        isConfirmPasswordEqualPassword(password, confirmPasswords);
     }
     public static boolean authorization(String login, String password, String confirmPassword) {
         try {
--- src/lection2/Main.java
+++ src/lection2/Main.java
@@ -1,7 +1,19 @@
 package lection2;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
 
+        System.out.println("Please enter the login");
+        String login = in.next();
+        System.out.println("Please enter the password");
+        String password = in.next();
+        System.out.println("Please enter the password again");
+        String confirmPassword = in.next();
+
+        boolean isDataCorrect = Authorization.authorization(login, password, confirmPassword);
+        System.out.println(isDataCorrect);
     }
 }
