137ae1ddc86592c1f91e0df811e0b49eaf608f28
Ivan Kabanov <i.borkoff@yandex.ru>
2024-11-02 11:06:12 +0300

    Template for class Student has been created, point 1 has been completed

---
+++ src/lection4/Student.java
@@ -0,0 +1,21 @@
+package lection4;
+
+import java.util.Map;
+
+public class Student {
+    private String name;
+    private String group;
+    private int course;
+    private Map<String, Integer> results;
+    public Student(
+            String name,
+            String group,
+            int course,
+            Map<String, Integer> results
+    ) {
+        this.name = name;
+        this.group = group;
+        this.course = course;
+        this.results = results;
+    }
+}
