9b5525dfa0d8a7dd68b44b7b0e8967214fcc4f60
Ivan Kabanov <i.borkoff@yandex.ru>
2024-11-24 00:01:09 +0300

    add test_auth_success

---
+++ maven_project/src/test/java/HomeworkTest.java
@@ -0,0 +1,90 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.By;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
+
+public class HomeworkTest {
+
+    @BeforeClass
+    public void init() {
+        Configuration.browser = "edge";
+        Configuration.baseUrl = "https://web.telegram.org/a/";
+    }
+
+    /**
+     * Для тестирования был выбран сайт https://web.telegram.org/a/
+     * Сайт тестировался по трем сценариям:
+     * 1. Успешная авторизация на сайте
+     *      - Попадаем на сайт
+     *      - Выбираем "Продолжить на русском"
+     *      - Нажимаем "Вход по номеру телефона"
+     *      - Затем вводим верный номер телефона
+     *      - Нажимаем кнопку "Далее"
+     *      - Затем проверяем, что мы на странице, на которой нужно ввести код
+     *      (Так как для входа в телеграм нужны реальные данные, то за "успешную авторизацию" будем считать попадание на страницу с вводом кода для входа в аккаунт)
+     * 2. Неуспешная авторизация на сайте
+     *      - Попадаем на сайт
+     *      - Выбираем "Продолжить на русском"
+     *      - Нажимаем "Вход по номеру телефона"
+     *      - Затем вводим неверный номер телефона
+     *      - Нажимаем кнопку "Далее"
+     *      - Затем проверяем, что в форме ввода номера появилась надпись "Invalid phone number"
+     * 3. Успешная авторизация на сайте через номер другой страны
+     *      - Попадаем на сайт
+     *      - Нажимаем "Log in by phone number"
+     *      - Затем выбираем любую страну
+     *      - Нажимаем кнопку "Next"
+     *      - Затем проверяем, что мы на странице, на которой нужно ввести код
+     */
+
+    private static final String H1_ENG = "Log in to Telegram by QR Code";
+    private static final String H1_RUS = "Быстрый вход по QR-коду";
+    private static final String LOG_IN_ENG = "Log in by phone Number";
+    private static final String LOG_IN_RUS = "Вход по номеру телефона";
+    private static final String CHANGE_LANG_BTN_TEXT = "Продолжить на русском";
+    private static final String INPUT_ID = "sign-in-phone-number";
+    private static final String INPUT_NUMBER = "79876886998";
+
+    @Test(groups = "homework")
+    public void test_auth_success() {
+        open(Configuration.baseUrl);
+
+        SelenideElement h1_element = $(By.cssSelector("h1"));
+        SelenideElement logInButton = $x("//button[text()='" + LOG_IN_ENG + "']");
+        SelenideElement changeLangButton = $x("//button[text()='" + CHANGE_LANG_BTN_TEXT + "']");
+
+        h1_element
+                .shouldBe(visible)
+                .shouldHave(text(H1_ENG));
+        logInButton.shouldBe(visible);
+        changeLangButton.shouldBe(visible);
+
+        changeLangButton.click();
+
+        h1_element.shouldHave(text(H1_RUS));
+        logInButton = $x("//button[text()='" + LOG_IN_RUS + "']");
+        logInButton.
+                shouldBe(visible)
+                .click();
+
+        SelenideElement input = $(By.id(INPUT_ID));
+        SelenideElement next_button = $(By.cssSelector("button[type=\"submit\"]"));
+
+        input.shouldBe(visible).click();
+        next_button.shouldNotBe(exist);
+
+        input.type(INPUT_NUMBER);
+
+        next_button = $(By.cssSelector("button[type=\"submit\"]"));
+        next_button.shouldBe(visible);
+        next_button.click();
+
+        $x("//*[@id=\"monkey\"]").shouldBe(visible);
+        sleep(1500);
+    }
+}
