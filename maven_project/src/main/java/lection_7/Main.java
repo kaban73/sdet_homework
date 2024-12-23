16d387520b9f6b8122e8d94257f37fa6d7cb51cc
Ivan Kabanov <i.borkoff@yandex.ru>
2024-12-23 16:48:38 +0300

    init lection_7

--- maven_project/src/main/java/lection_7/Main.java
+++ maven_project/src/main/java/lection_7/Main.java
@@ -1,4 +1,4 @@
-package lection_6;
+package lection_7;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
